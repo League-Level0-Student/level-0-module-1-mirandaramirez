df664e72b347c8b3acc80fdd858fe7cd736ebdc8
ilabstaff <ilabstaff@172.16.116.71>
2019-08-24 10:57:08 -0700

    remarcable

---
+++ src/_02_unbirthday/remarcable.java
@@ -0,0 +1,31 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class remarcable {
+public static void main(String[] args) {
+	String MariStar="has wavy hair";
+	String PinkFact="is tall";
+	String Jessica="has brown eyes";
+	String input=JOptionPane.showInputDialog("whose name is longer then mariene's ");
+	if(input.equals("MariStar")) {
+	JOptionPane.showMessageDialog(null,"MariStar thing is"+ MariStar);	
+	if(input.equals("pinkFact")){
+	JOptionPane.showMessageDialog(null,"PinkFact thing is"+ PinkFact);	
+	if(input.equals("Jessica")){
+	JOptionPane.showMessageDialog(null,"Jessica thing is"+ Jessica);
+	
+	
+	
+	
+	
+	}
+	
+
+	
+	
+	
+	
+	
+}
+}	
