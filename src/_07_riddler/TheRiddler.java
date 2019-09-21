f0e214167bb444f55e4d8ebd74b5af07bfd299cc
ilabstaff <ilabstaff@172.16.116.68>
2019-09-21 10:58:39 -0700

    riddler

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -14,11 +14,16 @@ public class TheRiddler {
 		// 1. Make a variable to hold the score
 		int score=0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-		JOptionPane.showMessageDialog(null,"there are 10 fishes 2 drown 4 swim away and 3 die how many are left");
+		String answer=JOptionPane.showInputDialog("there are 10 fishes 2 drown 4 swim away and 3 die how many are left");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-		String answer=
+		if(answer.equals("there is 1 left")) {
+		score++;
+		JOptionPane.showMessageDialog(null,"correct");
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+		else {
+        	
+        }
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
